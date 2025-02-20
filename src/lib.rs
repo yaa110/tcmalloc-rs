1d07a274904c8ad2cc43c6bed0ebfcc194b8790c
Navid <yaa110@gmail.com>
2025-02-20 19:37:34 +0100

    edition: 2024

--- src/lib.rs
+++ src/lib.rs
@@ -6,18 +6,18 @@ pub struct TcMalloc;
 
 unsafe impl GlobalAlloc for TcMalloc {
     unsafe fn alloc(&self, layout: Layout) -> *mut u8 {
-        bindings::tc_malloc(layout.size()) as *mut u8
+        unsafe { bindings::tc_malloc(layout.size()) as *mut u8 }
     }
 
     unsafe fn dealloc(&self, ptr: *mut u8, _layout: Layout) {
-        bindings::tc_free(ptr as *mut _)
+        unsafe { bindings::tc_free(ptr as *mut _) }
     }
 
     unsafe fn alloc_zeroed(&self, layout: Layout) -> *mut u8 {
-        bindings::tc_calloc(layout.size(), 1) as *mut u8
+        unsafe { bindings::tc_calloc(layout.size(), 1) as *mut u8 }
     }
 
     unsafe fn realloc(&self, ptr: *mut u8, _layout: Layout, new_size: usize) -> *mut u8 {
-        bindings::tc_realloc(ptr as *mut _, new_size) as *mut u8
+        unsafe { bindings::tc_realloc(ptr as *mut _, new_size) as *mut u8 }
     }
 }
